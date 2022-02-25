fb0d61fc5f8a7502cbb2b72a45d833626040770f
Shem Skillman <43577362+ShemSkillman@users.noreply.github.com>
2022-02-25 16:36:42 +0000

    Working spotlight

--- Project_Template/scenebasic_uniform.cpp
+++ Project_Template/scenebasic_uniform.cpp
@@ -17,44 +17,26 @@ using std::endl;
 
 using glm::vec3;
 using glm::mat4;
+using glm::vec4;
+using glm::mat3;
 
-SceneBasic_Uniform::SceneBasic_Uniform() : plane(10.0f, 10.0f, 100, 100)
+SceneBasic_Uniform::SceneBasic_Uniform() : plane(50.0f, 50.0f, 1, 1), teapot(14, glm::mat4(1.0f)), torus(1.75f * 0.75f, 0.75f * 0.75f, 50, 50)
 {
 	mesh = ObjMesh::load("../Project_Template/media/pig_triangulated.obj", true);
 }
 
 void SceneBasic_Uniform::initScene()
 {
-    compile();
-
+	compile();
 	glEnable(GL_DEPTH_TEST);
 
-	model = mat4(1.0f);
-	
-	prog.setUniform("ViewPosition", vec3(0.5f, 0.75f, 0.75f));
-
-	view = glm::lookAt(vec3(0.5f, 0.75f, 0.75f), vec3(0.0f, 0.0f, 0.0f),
-		vec3(0.0f, 1.0f, 0.0f));
+	view = glm::lookAt(vec3(5.0f, 5.0f, 7.5f), vec3(0.0f, 0.75f, 0.0f), vec3(0.0f,
+		1.0f, 0.0f));
 	projection = mat4(1.0f);
-
-	float x, z;
-	for (int i = 0; i < 3; i++)
-	{
-		std::stringstream name;
-		name << "lights[" << i << "].Position";
-		x = 2.0f * cosf((glm::two_pi<float>() / 3) * i);
-		z = 2.0f * sinf((glm::two_pi<float>() / 3) * i);
-		prog.setUniform(name.str().c_str(), view * glm::vec4(x, 1.2f, z +
-			1.0f, 1.0f));
-	}
-
-	prog.setUniform("lights[0].L", vec3(0.8f));
-	prog.setUniform("lights[1].L", vec3(0.4f));
-	prog.setUniform("lights[2].L", vec3(0.6f));
-
-	prog.setUniform("lights[0].La", vec3(0.2f));
-	prog.setUniform("lights[1].La", vec3(0.2f));
-	prog.setUniform("lights[2].La", vec3(0.2f));
+	prog.setUniform("Spot.L", vec3(0.9f));
+	prog.setUniform("Spot.La", vec3(0.5f));
+	prog.setUniform("Spot.Exponent", 50.0f);
+	prog.setUniform("Spot.Cutoff", glm::radians(15.0f));
 }
 
 void SceneBasic_Uniform::compile()
@@ -77,24 +59,43 @@ void SceneBasic_Uniform::update( float t )
 void SceneBasic_Uniform::render()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	prog.setUniform("Material.Kd", 1.0f, 0.76f, 0.15f);
-	prog.setUniform("Material.Ks", 1.0f, 1.0f, 1.0f);
-	prog.setUniform("Material.Ka", 0.50f, 0.38f, 0.075f);
-	prog.setUniform("Material.Shininess", 180.0f);
+
+	vec4 lightPos = vec4(0.0f, 10.0f, 0.0f, 1.0f);
+	prog.setUniform("Spot.Position", view * lightPos);
+	mat3 normalMatrix = mat3(vec3(view[0]), vec3(view[1]), vec3(view[2]));
+
+	prog.setUniform("Spot.Direction", normalMatrix * vec3(-lightPos));
+	prog.setUniform("Material.Kd", 0.2f, 0.55f, 0.9f);
+	prog.setUniform("Material.Ks", 0.95f, 0.95f, 0.95f);
+	prog.setUniform("Material.Ka", 0.2f * 0.3f, 0.55f * 0.3f, 0.9f * 0.3f);
+	prog.setUniform("Material.Shininess", 100.0f);
 
 	model = mat4(1.0f);
-	model = glm::rotate(model, glm::radians(90.0f), vec3(0.0f, 1.0f, 0.0f));
+	model = glm::translate(model, vec3(0.0f, 0.0f, -2.0f));
+	model = glm::rotate(model, glm::radians(45.0f), vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(-90.0f), vec3(1.0f, 0.0f, 0.0f));
+
 	setMatrices();
-	mesh->render();
+	teapot.render();
 
-	prog.setUniform("Material.Kd", 0.19f, 0.35f, 0.09f);
-	prog.setUniform("Material.Ks", 0.1f, 0.1f, 0.1f);
-	prog.setUniform("Material.Ka", 0.19f, 0.35f, 0.09f);
+	prog.setUniform("Material.Kd", 0.2f, 0.55f, 0.9f);
+	prog.setUniform("Material.Ks", 0.95f, 0.95f, 0.95f);
+	prog.setUniform("Material.Ka", 0.2f * 0.3f, 0.55f * 0.3f, 0.9f * 0.3f);
+	prog.setUniform("Material.Shininess", 100.0f);
+
+	model = mat4(1.0f);
+	model = glm::translate(model, vec3(-1.0f, 0.75f, 3.0f));
+	model = glm::rotate(model, glm::radians(-90.0f), vec3(1.0f, 0.0f, 0.0f));
+
+	setMatrices();
+	torus.render();
+
+	prog.setUniform("Material.Kd", 0.7f, 0.7f, 0.7f);
+	prog.setUniform("Material.Ks", 0.9f, 0.9f, 0.9f);
+	prog.setUniform("Material.Ka", 0.2f, 0.2f, 0.2f);
 	prog.setUniform("Material.Shininess", 180.0f);
 
 	model = mat4(1.0f);
-	model = glm::translate(model, vec3(0.0f, -0.45f, 0.0f));
 
 	setMatrices();
 	plane.render();
